8937bb43443c63fd00d83b9fcbd579b9e98671f1
Sebastian Sanchez <ossanchezm@gmail.com>
2023-01-10 13:11:48 -0600

    sidebar

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const videoDetail = () => {
-  return (
-    <div>videoDetail</div>
-  )
-}
+const VideoDetail = () => {
+  return <div>videoDetail</div>;
+};
 
-export default videoDetail
+export default VideoDetail;
