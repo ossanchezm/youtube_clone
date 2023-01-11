d4737dc824a04cb94797bc21e07203fe959bd16b
Sebastian Sanchez <ossanchezm@gmail.com>
2023-01-11 00:09:07 -0600

    fixed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -21,8 +21,9 @@ const VideoCard = ({
     <Card
       sx={{
         width: {
-          md: "320px",
           xs: "100%",
+          sm: "358px",
+          md: "320px",
           boxShadow: "none",
           borderRadius: 0,
         },
@@ -32,7 +33,14 @@ const VideoCard = ({
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 359, height: 180 }}
+          sx={{
+            width: {
+              xs: "100%",
+              sm: "358px",
+              md: "320px",
+            },
+            height: 180,
+          }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
